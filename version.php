71e580e53c7890c51a50879db6b1135a7fc4f1c4
Vicho <Vicho@10.50.114.23>
2017-01-25 16:14:05 -0400

    actualizacion version

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2015012300; 
+$plugin->version = 2017012501; 
 $plugin->requires = 2010021900;
 $plugin->component = 'local_tutoriales';
 $plugin->pluginname = "local_tutoriales";
